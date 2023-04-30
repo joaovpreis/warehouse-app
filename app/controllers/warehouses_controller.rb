26a6c3432f05416b139eaad649a9d827f6aa69f6
Joao Reis <joaovreis015@gmail.com>
2023-04-30 19:41:04 -0300

    Cria o formulário para cadastro de novos galpões

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -6,4 +6,23 @@ class WarehousesController < ApplicationController
   def show
     @warehouse = Warehouse.find(params[:id])
   end
+
+  def new
+    @warehouse = Warehouse.new
+  end
+
+  def create
+    @warehouse = Warehouse.new warehouse_params
+
+    if @warehouse.save
+      redirect_to root_path and return
+    end
+
+    render 'new'
+
+  end
+
+  def warehouse_params 
+    params.require(:warehouse).permit(:name, :code, :city, :area, :cep, :address, :description)
+  end
 end
---
+++ spec/system/user_view_warehouse_create_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe 'Usuario acessa o formulário de cadastro de um novo armazém' do
+  it 'e verifica existência dos campos corretos' do
+    visit new_warehouse_path
+
+    expect(page).to have_field('Nome')
+    expect(page).to have_field('Código')
+    expect(page).to have_field('Cidade')
+    expect(page).to have_field('Área')
+    expect(page).to have_field('CEP')
+    expect(page).to have_field('Endereço')
+    expect(page).to have_field('Descrição')
+    expect(page).to have_button('Cadastrar Galpão')
+  end
+
+  it 'e cadastra um armazém com sucesso' do
+    visit new_warehouse_path
+    
+    fill_in 'Nome',	with: 'Aeroporto Rio'
+    fill_in 'Código',	with: 'SDU'
+    fill_in 'Cidade',	with: 'Rio de Janeiro'
+    fill_in 'Área',	with: '300_000'
+    fill_in 'CEP',	with: '25000-320'
+    fill_in 'Endereço',	with: 'Avenida Brasil, 1500'
+    fill_in 'Descrição',	with: 'Galpao privado do aeroporto Santos Dumont'
+    click_on 'Cadastrar Galpão'
+
+    expect(page).to have_current_path(root_path) 
+    expect(page).to have_content('Nome: Aeroporto Rio')
+    expect(page).to have_content('Cidade: Rio de Janeiro')
+    expect(page).to have_content('Área: 300000 m2')
+
+  end
+  
+  
+end
